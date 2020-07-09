a8cdca61473798450e2325ee3722310436e1eb69
김미희 <opalgml@naver.com>
2020-07-09 19:42:27 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200709/MainActivity.kt
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.lifecycle_20200709
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        goToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this, OtherActivity::class.java)
+
+            startActivity(myIntent)
+        }
+
+    }
+
+        override fun onResume() {
+        super.onResume()
+            Log.d("메인화면", "onResume 실행")
+        }
+}
